64d068b1462c2f837edbec4ea50b33218fb5f80f
yaena1223 <rewrite_the_stars_@naver.com>
2022-02-09 23:19:40 +0900

    그리디

--- 이코테/그리디_(2)큰수의법칙.py
+++ 이코테/그리디_(2)큰수의법칙.py
@@ -1,5 +1,16 @@
 n, m, k = map(int,input().split())
 array = list(map(int,input().split()))
 array.sort()
-cnt = 0
-while(cnt<m):
+n1 = array[-1]
+n2 = array[-2]
+ans = 0
+while True:
+    for i in range(k):
+        ans = ans+ n1
+        m = m-1
+    ans = ans + n2
+    m = m -1
+    if m==0 :
+        break
+
+print(ans)
