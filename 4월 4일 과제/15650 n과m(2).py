e77fa6439fda6786ab6b1ff31470ea576e666c97
yaena1223 <yaena1223@dgu.ac.kr>
2022-04-04 02:23:16 +0900

    4월 4일 과제

--- 4월 4일 과제/15650 n과m(2).py
+++ 4월 4일 과제/15650 n과m(2).py
@@ -1,1 +1,18 @@
 n, m = map(int,input().split())
+arr = [0]*m
+visit = [False]*(n+1)
+def back(x):
+    if x == m:
+        print(*arr)
+        return
+    for i in range(1,n+1):
+        if not visit[i]:
+            if arr[x - 1] < i:
+                arr[x] = i
+                visit[i] = True
+                back(x+1)
+                arr[x]=0
+                visit[i] = False
+
+
+back(0)
