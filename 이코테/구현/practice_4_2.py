e068bb4bfbb848c076e78c0e103fd4ae71d4dabb
yaena1223 <yaena1223@dgu.ac.kr>
2022-09-02 03:11:02 +0900

    09/02 이것이 코딩테스트다 구현 연습문제 4-2

---
+++ 이코테/구현/practice_4_2.py
@@ -0,0 +1,11 @@
+n = int(input())
+h,m,s = 0,0,0
+ans = 0
+
+for i in range(n+1):
+    for j in range(60):
+        for z in range(60):
+            if '3' in str(i) or '3' in str(j) or '3' in str(z):
+                ans += 1
+
+print(ans)
